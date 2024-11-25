e9d5668658c9d4e25333e72633f1c7607a118033
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2024-11-25 23:04:32 +0100

    add toast after add to cart

--- frontend/src/utils/functions.ts
+++ frontend/src/utils/functions.ts
@@ -1,4 +1,7 @@
+import { createStandaloneToast } from "@chakra-ui/react";
 import { IProduct } from "../interface";
+
+const { toast } = createStandaloneToast();
 
 export const addItemsToShoppingCart = (
   cartItems: IProduct[],
@@ -6,9 +9,22 @@ export const addItemsToShoppingCart = (
 ) => {
   const exist = cartItems.find((item) => item.id === product.id);
   if (exist) {
+    toast({
+      title: "Added to your Cart.",
+      description: "This item already exists, the quantity will be increased.",
+      status: "success",
+      duration: 2000,
+      isClosable: true,
+    });
     return cartItems.map((item) =>
       item.id === product.id ? { ...item, qty: item.qty + 1 } : item
     );
   }
+  toast({
+    title: "Added to your Cart.",
+    status: "success",
+    duration: 2000,
+    isClosable: true,
+  });
   return [...cartItems, { ...product, qty: 1 }];
 };
