64ce24d97878f9bb822ceee69b513d6f39b99c74
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2024-11-26 17:08:00 +0100

    add shared alert dialog component

--- frontend/src/components/DashboardProductsTable.tsx
+++ frontend/src/components/DashboardProductsTable.tsx
@@ -10,7 +10,7 @@ import {
   Th,
   Thead,
   Tr,
-  useColorModeValue,
+  useDisclosure,
 } from "@chakra-ui/react";
 import TableSkeleton from "./TableSkeleton";
 import { useGetDashboardProductsQuery } from "../app/services/apiSlice";
@@ -20,104 +20,116 @@ import { AiOutlineEye } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { BsTrash } from "react-icons/bs";
 import { FiEdit } from "react-icons/fi";
+import CustomAlertDialog from "../shared/AlertDialog";
 
 const DashboardProductsTable = () => {
   const { isLoading, data, error } = useGetDashboardProductsQuery({ page: 1 });
+  const { isOpen, onOpen, onClose } = useDisclosure();
   // console.log(data);
   if (isLoading) return <TableSkeleton />;
-
   return (
-    <TableContainer
-      maxW={"100%"}
-      mx={"auto"}
-      css={{
-        "&::-webkit-scrollbar": {
-          height: "10px",
-        },
-        "&::-webkit-scrollbar-thumb": {
-          background: `${useColorModeValue("#2d3748", "#0987a0")}`,
-        },
-        "&::-webkit-scrollbar-thumb:hover": {
-          background: `${useColorModeValue("#2d37ff", "#0987cd")}`,
-        },
-      }}
-    >
-      <Table variant="simple">
-        <TableCaption>Imperial to metric conversion factors</TableCaption>
-        <Thead>
-          <Tr>
-            <Th isNumeric>ID</Th>
-            <Th>TITLE</Th>
-            <Th>CATEGORY</Th>
-            <Th>THUMBNAIL</Th>
-            <Th isNumeric>PRICE</Th>
-            <Th isNumeric>STOCK</Th>
-            <Th>ACTION</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data.data.map((item: IProduct) => (
-            <Tr key={item.id}>
-              <Td isNumeric>{item.id}</Td>
-              <Td>{item.attributes.title}</Td>
-              <Td>{item.attributes.category.data.attributes.title}</Td>
-              <Td>
-                <Image
-                  src={`${import.meta.env.VITE_SERVER_URL}${item.attributes.thumbnail?.data?.attributes?.url}`}
-                  alt={item.attributes.title}
-                  w={"80px"}
-                  h={"80px"}
-                  rounded={"full"}
-                  // objectFit={"cover"}
-                  fallbackSrc={imgFalBack}
-                />
-              </Td>
-              <Td isNumeric>{item.attributes.price}</Td>
-              <Td isNumeric>{item.attributes.stock}</Td>
-              <Td>
-                <Button
-                  as={Link}
-                  to={`/products/${item.id}`}
-                  colorScheme="purple"
-                  variant={"solid"}
-                  mr={3}
-                  onClick={() => {}}
-                >
-                  <AiOutlineEye size={17} />
-                </Button>
-                <Button
-                  colorScheme="red"
-                  variant={"solid"}
-                  mr={3}
-                  onClick={() => {}}
-                >
-                  <BsTrash size={17} />
-                </Button>
-                <Button
-                  colorScheme="blue"
-                  variant={"solid"}
-                  mr={3}
-                  onClick={() => {}}
-                >
-                  <FiEdit size={17} />
-                </Button>
-              </Td>
+    <>
+      <TableContainer
+        maxW={"100%"}
+        mx={"auto"}
+        css={{
+          "&::-webkit-scrollbar": {
+            height: "10px",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            background: "#0987a0",
+          },
+          "&::-webkit-scrollbar-thumb:hover": {
+            background: "#0987cd",
+          },
+        }}
+      >
+        <Table variant="simple">
+          <TableCaption>Imperial to metric conversion factors</TableCaption>
+          <Thead>
+            <Tr>
+              <Th isNumeric>ID</Th>
+              <Th>TITLE</Th>
+              <Th>CATEGORY</Th>
+              <Th>THUMBNAIL</Th>
+              <Th isNumeric>PRICE</Th>
+              <Th isNumeric>STOCK</Th>
+              <Th>ACTION</Th>
             </Tr>
-          ))}
-        </Tbody>
-        <Tfoot>
-          <Tr>
-            <Th isNumeric>ID</Th>
-            <Th>TITLE</Th>
-            <Th>CATEGORY</Th>
-            <Th>THUMBNAIL</Th>
-            <Th isNumeric>PRICE</Th>
-            <Th isNumeric>STOCK</Th>
-            <Th>ACTION</Th>
-          </Tr>
-        </Tfoot>
-      </Table>
-    </TableContainer>
+          </Thead>
+          <Tbody>
+            {data.data.map((item: IProduct) => (
+              <Tr key={item.id}>
+                <Td isNumeric>{item.id}</Td>
+                <Td>{item.attributes.title}</Td>
+                <Td>{item.attributes.category.data.attributes.title}</Td>
+                <Td>
+                  <Image
+                    src={`${import.meta.env.VITE_SERVER_URL}${item.attributes.thumbnail?.data?.attributes?.url}`}
+                    alt={item.attributes.title}
+                    w={"80px"}
+                    h={"80px"}
+                    rounded={"full"}
+                    // objectFit={"cover"}
+                    fallbackSrc={imgFalBack}
+                  />
+                </Td>
+                <Td isNumeric>{item.attributes.price}</Td>
+                <Td isNumeric>{item.attributes.stock}</Td>
+                <Td>
+                  <Button
+                    as={Link}
+                    to={`/products/${item.id}`}
+                    colorScheme="purple"
+                    variant={"solid"}
+                    mr={3}
+                    target="_blank"
+                    onClick={() => {}}
+                  >
+                    <AiOutlineEye size={17} />
+                  </Button>
+                  <Button
+                    colorScheme="red"
+                    variant={"solid"}
+                    mr={3}
+                    onClick={onOpen}
+                  >
+                    <BsTrash size={17} />
+                  </Button>
+                  <Button
+                    colorScheme="blue"
+                    variant={"solid"}
+                    mr={3}
+                    onClick={() => {}}
+                  >
+                    <FiEdit size={17} />
+                  </Button>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+          <Tfoot>
+            <Tr>
+              <Th isNumeric>ID</Th>
+              <Th>TITLE</Th>
+              <Th>CATEGORY</Th>
+              <Th>THUMBNAIL</Th>
+              <Th isNumeric>PRICE</Th>
+              <Th isNumeric>STOCK</Th>
+              <Th>ACTION</Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
+      <CustomAlertDialog
+        isOpen={isOpen}
+        onClose={onClose}
+        title={"Are you sure?"}
+        description="Do you really want to destroy this product/ This product cannot be undone."
+        okTxt="Destroy"
+        variant="outline"
+      />
+    </>
   );
 };
 
---
+++ frontend/src/shared/AlertDialog.tsx
@@ -0,0 +1,62 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+} from "@chakra-ui/react";
+import { useRef } from "react";
+
+interface IProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  description: string;
+  cancelTxt?: string;
+  okTxt?: string;
+  variant?: "ghost" | "link" | "outline" | "solid" | "unstyled";
+}
+
+function CustomAlertDialog({
+  isOpen,
+  onClose,
+  title,
+  description,
+  cancelTxt = "Cancel",
+  okTxt = "Ok",
+  variant = "solid",
+}: IProps) {
+  const cancelRef = useRef<HTMLButtonElement | null>(null);
+
+  return (
+    <>
+      <AlertDialog
+        motionPreset="slideInBottom"
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+      >
+        <AlertDialogOverlay />
+
+        <AlertDialogContent>
+          <AlertDialogHeader>{title}</AlertDialogHeader>
+          <AlertDialogCloseButton />
+          <AlertDialogBody>{description}</AlertDialogBody>
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              {cancelTxt}
+            </Button>
+            <Button colorScheme="red" ml={3} variant={variant}>
+              {okTxt}
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
+}
+export default CustomAlertDialog;
